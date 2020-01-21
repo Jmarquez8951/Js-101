8d5dd3a4b343c6d1602de261df7811850bb1a913
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-01-20 18:49:35 -0600

    nugget function

---
+++ functions.js
@@ -0,0 +1,12 @@
+console.log("functions")
+
+const nuggetizer = (animal) => {
+    const output = `processed ${animal}`;
+    return output;
+};
+
+console.log(nuggetizer('Chicken'));
+console.log(nuggetizer('Cow'));
+console.log(nuggetizer('Fish'));
+console.log(nuggetizer('Pig'));
+console.log(nuggetizer('tire'));
