550a36bc178317b4704b01129e8db44d451f4b7a
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-01-20 18:53:15 -0600

    added dog breed function

--- functions.js
+++ functions.js
@@ -10,3 +10,12 @@ console.log(nuggetizer('Cow'));
 console.log(nuggetizer('Fish'));
 console.log(nuggetizer('Pig'));
 console.log(nuggetizer('tire'));
+
+
+const dogBreed = (dog) => {
+    const output = `My favorite dog breed is ${dog}.`;
+    return output;
+};
+
+
+console.log(dogBreed('lab'));
