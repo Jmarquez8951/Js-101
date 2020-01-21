5a05dabaf1cb533a27a704f564f039ef617bce90
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-01-20 20:45:41 -0600

    finished up the challenges in objects

--- object.js
+++ object.js
@@ -19,3 +19,40 @@ const ageCheck = (age) => {
 
 ageCheck(12);
 ageCheck(24);
+
+// Objects
+
+const expense = {
+    dateCreated: '01/17/2020',
+    location: 'Baja Burrito',
+    cost: 12.57,
+    isGood: true
+};
+
+// dot notation
+console.log(expense.cost);
+
+// bracket notation
+console.log(expense['cost']);
+
+const employee = {
+    firstName: 'John',
+    lastName: 'Doe',
+    gender: 'male',
+    age: 52,
+    experience: '5 years',
+    position: 'Regional Manager'
+}
+
+// challenge
+
+const statusMaker = (person) => {
+    if (person.firstName[0] === 'M') {
+        person.status = "vip";
+    } else {
+        person.status = "peasant";
+    }
+    return person;
+}
+
+console.log(statusMaker(employee));
